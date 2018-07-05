f351362342a26017a9a8233b770168859737e13a
rongliu <rongliu@thoughtworks.com>
2018-07-05 17:37:55 +0800

    add eslint

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ import {login} from './login/login'
 app.get('/api', login)
 
 app.get('/api/hello', (req, res) =>
-    res.status(200).send({
-      id: '123',
-    })
-  )
+  res.status(200).send({
+    id: '123',
+  })
+)
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT)
