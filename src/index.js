6fc1da24a7983a300d807d382edc8bc53725a0a9
rongliu <rongliu@thoughtworks.com>
2018-07-05 14:54:56 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const PORT = process.env.PORT || 3000
+
+express()
+  .get('/', (req, res) =>
+    res.status(200).send({
+      message: 'Welcome tod the beginning of nothingness.',
+    })
+  )
+
+app.get('/hello', (req, res) =>
+    res.status(200).send({
+      id: '123',
+    })
+  )
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
